5fc9097b558967d26b18de55d7c7235dcaf1c045
HalfWing_lab <halfwing1998@gmail.com>
2021-09-14 23:59:14 +0900

    until api

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { Alert } from "react-native";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
 import Loading from "./Loading";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
+import Weather from "./Weather";
 
 
+const API_KEY = "c870f23d31f768a39765bb0ec06e9603";
 
 export default class extends React.Component{
+  state = {
+    isLoading : true,
+  };
+  getWeather = async(latitude, longitude) => {
+   const {data} = await axios.get(
+     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+     ); 
+     this.setState({isLoading:false, temp: data.main.temp})
+     //console.log(data);
+ };
   getLocation = async() => {
     try{
+      //throw Error();
       await Location.requestForegroundPermissionsAsync();
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      const {coords : {latitude, longitude}
+    } = await Location.getCurrentPositionAsync();
+    this.getWeather(latitude, longitude);
+    this.setState({isLoading : false});
+      // Send to API and get weather!
+
     }catch(error){
-      Alert.alert("Can't find you. ","So sad");
+      Alert.alert("Can't find your position. ","Please Check again");
     }
     
   };
@@ -20,6 +38,7 @@ export default class extends React.Component{
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const {isLoading,temp} = this.state;
+    return isLoading ? <Loading /> :<Weather temp = {temp}/>;
   }
 }
---
+++ Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import Proptypes from "prop-types";
+
+export default Weather({temp}){
+    return <View><Text></Text></View>
+}
+
+Weather.propTypes={
+
+}
